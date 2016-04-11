415fea645644eb0722d255fc72358675942f42cf
colt <necromanga@gmail.com>
2016-04-11 11:46:20 +0200

    Menu.

--- core/src/com/colt/flappyponies/states/StateMenu.java
+++ core/src/com/colt/flappyponies/states/StateMenu.java
@@ -1,6 +1,8 @@
 package com.colt.flappyponies.states;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.colt.flappyponies.Boot;
 
 /**
  * Created by colt on 4/11/16.
@@ -8,8 +10,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class StateMenu extends State {
 
+    private Texture background;
+    private Texture playbutton;
+
     public StateMenu(GameStateManager gameStateManager) {
         super(gameStateManager);
+        background = new Texture("graphics/background.png");
+        playbutton = new Texture("graphics/playbutton.png");
+
     }
 
     @Override
@@ -24,7 +32,10 @@ public class StateMenu extends State {
 
     @Override
     public void render(SpriteBatch spriteBatch) {
-
+        spriteBatch.begin();
+        spriteBatch.draw(background, 0, 0, Boot.WIDTH, Boot.HEIGHT); //Remember, BOTTOM LEFT!
+        spriteBatch.draw(playbutton, (Boot.WIDTH / 2) - (playbutton.getWidth() / 2), (Boot.HEIGHT / 2) - (playbutton.getHeight() / 2));
+        spriteBatch.end();
     }
 
 }
